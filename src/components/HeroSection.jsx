a9de0287b271155558516b6a6af4628fbc570c20
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2022-12-19 19:56:23 +0100

    Add hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+import HeroImage from '../assets/images/hero-image.png'
+
+export default function Navbar() {
+    return (
+        <section id='Hero'>
+            <img src={HeroImage} />
+            <h1>Hello, I am Sylvia a frontend developer from Egypt</h1>
+        </section>
+    );
+}
