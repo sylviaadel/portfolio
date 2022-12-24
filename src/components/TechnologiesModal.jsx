900a6f7b313735c2912edc6432320de63d71efd4
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2022-12-24 23:38:05 +0100

    Add static content to technologies modal and fix styles

--- src/components/TechnologiesModal.jsx
+++ src/components/TechnologiesModal.jsx
@@ -1,4 +1,6 @@
 import ReactDom from "react-dom";
+import closeIcon from './../assets/images/close.svg'
+import todoImage from './../assets/images/todoImage.png'
 
 export default function TechnologiesModal({open, onClose}) {
     if (!open) return null
@@ -7,9 +9,12 @@ export default function TechnologiesModal({open, onClose}) {
         <>
             <div className="modal-overlay"></div>
             <div id='TechnologiesModal' className="modal">
-                <button type="button" class="close" onClick={onClose}>
-                    <span aria-hidden="true">&times;</span>
+                <button type="button" className="close" onClick={onClose}>
+                    <img src={closeIcon} alt="Cross Icon to close modal" />
                 </button>
+                <img  src={todoImage} alt="React Logo" />
+                <h2>React</h2>
+                <p>I used this technology in many projects, I was responsible for building the app from scratch using react and adding the components and linking them together along with adding the styles needed.</p>
             </div>
         </>,
         document.getElementById('portal')
