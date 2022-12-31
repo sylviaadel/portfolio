7a2f94df631f6d9b0c104753e2b1ede0a224808f
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2022-12-31 13:03:57 +0100

    Add last name

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <section id='About'>
             <div>
                 <h2>About Me</h2>
-                <p>Hello! I'm Sylvia, a frontend developer with eight years of experience in UI development. I am from Cairo, Egypt but currently I live in Stockholm, Sweden. </p>
+                <p>Hello! I'm Sylvia Tadross, a frontend developer with eight years of experience in UI development. I am from Cairo, Egypt but currently I live in Stockholm, Sweden. </p>
                 <p>This is my website as I want to showcase my work that I have done till now. Please check all details below and I will be super happy to hear from you soon!</p>
             </div>
             <img src={AboutImage} alt="A girl playing yoga and thinking about her intrests." />
