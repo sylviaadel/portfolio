5e24d1f318182e4b3cb8d0c445ee2257cb08cf0d
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-06 21:03:12 +0100

    add screenshots and new projects

--- src/components/ProjectsModal.jsx
+++ src/components/ProjectsModal.jsx
@@ -1,37 +1,50 @@
 import { useEffect } from "react";
 import ReactDom from "react-dom";
-import closeIcon from './../assets/images/close.svg'
+import closeIcon from "./../assets/images/close.svg";
 
-export default function ProjectsModal({open, onClose, project}) {
+export default function ProjectsModal({ open, onClose, project }) {
+  useEffect(() => {
+    if (open) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "unset";
+  }, [open]);
 
-    useEffect(() => {
-        if (open) document.body.style.overflow = 'hidden'
-        else document.body.style.overflow = 'unset';
-    }, [open]);
+  if (!open) return null;
 
-    if (!open) return null
-
-    return ReactDom.createPortal(
-        <>
-            <div className="modal-overlay" onClick={onClose}></div>
-            <div id='ProjectsModal' className="modal">
-                <button type="button" className="close" onClick={onClose}>
-                    <img src={closeIcon} alt="Cross Icon to close modal" />
-                </button>
-                <img src={`images/Projects/${project.image}`} alt={project.alt}/>
-                <div>
-                    <h2>{project.name}</h2>
-                    <p>{project.desc}</p>
-                    <ul className="tech-pills">
-                        {project.technologies.map((tech) => (
-                            <li>{tech.name}</li>
-                        ))}
-                    </ul>
-                    <a href={project.appLink} rel="noreferrer" target="_blank" className="btn-primary">Visit Website/App</a>
-                    <a href={project.gitLink} rel="noreferrer" target="_blank" className="btn-secondary">Git Repository</a>
-                </div>
-            </div>
-        </>,
-        document.getElementById('portal')
-    );
+  return ReactDom.createPortal(
+    <>
+      <div className="modal-overlay" onClick={onClose}></div>
+      <div id="ProjectsModal" className="modal">
+        <button type="button" className="close" onClick={onClose}>
+          <img src={closeIcon} alt="Cross Icon to close modal" />
+        </button>
+        <img src={`images/Projects/${project.image}`} alt={project.alt} />
+        <div>
+          <h2>{project.name}</h2>
+          <p>{project.desc}</p>
+          <ul className="tech-pills">
+            {project.technologies.map((tech) => (
+              <li>{tech.name}</li>
+            ))}
+          </ul>
+          <a
+            href={project.appLink}
+            rel="noreferrer"
+            target="_blank"
+            className="btn-primary"
+          >
+            Visit Website/App
+          </a>
+          <a
+            href={project.gitLink}
+            rel="noreferrer"
+            target="_blank"
+            className="btn-secondary"
+          >
+            Git Repository
+          </a>
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
 }
