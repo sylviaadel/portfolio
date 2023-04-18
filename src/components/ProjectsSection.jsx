57725f13f505c3a7b4984f3a702c5e35de655b3b
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-18 14:56:09 +0200

    Add netflix

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,42 +1,49 @@
-import { useState } from 'react'
-import project from './../models/ProjectItems.json';
-import ProjectsModal from './ProjectsModal'
+import { useState } from "react";
+import project from "./../models/ProjectItems.json";
+import ProjectsModal from "./ProjectsModal";
 
 export default function Projects() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [modalProject, setModalProject] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalProject, setModalProject] = useState({});
 
-    function initModal(project){
-        if (project.isDone === true){
-            setIsOpen(true);
-            setModalProject(project);
-        }
-        else{
-            return null
-        }
+  function initModal(project) {
+    if (project.isDone === true) {
+      setIsOpen(true);
+      setModalProject(project);
+    } else {
+      return null;
     }
+  }
 
-    const projectItems = project.map((project) => (
-        <li key={project.id} className="project-item" onClick={() => initModal(project)}>
-            <img src={`images/Projects/${project.image}`} alt={project.alt} />
-            <div className={project.isDone ? 'coming-soon-div hide' : "coming-soon-div"}>
-                <label>Coming Soon</label>
-            </div>
-            <h3>{project.name}</h3>
-        </li>
-    ));
-    return(
-        <>
-            <section id='Projects'>
-                <div>
-                    <h2>Projects</h2>
-                    <p>Here are the projects I have done and those I will be working on i Novare Potential.</p>
-                </div>
-                <ul className="projects-container">
-                    {projectItems}
-                </ul>
-            </section>
-            <ProjectsModal project={modalProject} open={isOpen} onClose={() => setIsOpen(false)} />
-        </>
-    );
+  const projectItems = project.map((project) => (
+    <li
+      key={project.id}
+      className="project-item"
+      onClick={() => initModal(project)}
+    >
+      <img src={`images/Projects/${project.image}`} alt={project.alt} />
+      <div
+        className={project.isDone ? "coming-soon-div hide" : "coming-soon-div"}
+      >
+        <label>Coming Soon</label>
+      </div>
+      <h3>{project.name}</h3>
+    </li>
+  ));
+  return (
+    <>
+      <section id="Projects">
+        <div>
+          <h2>Projects</h2>
+          <p>Here are the projects I have done with Novare Potential.</p>
+        </div>
+        <ul className="projects-container">{projectItems}</ul>
+      </section>
+      <ProjectsModal
+        project={modalProject}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+    </>
+  );
 }
--- src/components/TechnologiesSection.jsx
+++ src/components/TechnologiesSection.jsx
@@ -1,46 +1,63 @@
-import { useState } from 'react'
-import technology from './../models/TechnologyItems.json'
-import TechnologiesModal from './TechnologiesModal'
+import { useState } from "react";
+import technology from "./../models/TechnologyItems.json";
+import TechnologiesModal from "./TechnologiesModal";
 
 export default function Technologies() {
-    const [isOpen, setIsOpen] = useState(false);
-    const [modalTech, setModalTech] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalTech, setModalTech] = useState({});
 
-    function initModal(tech){
-        setIsOpen(true);
-        setModalTech(tech);
-    }
+  function initModal(tech) {
+    setIsOpen(true);
+    setModalTech(tech);
+  }
 
-    const knownTechnologyItems = technology.knownTech.map((technology) => (
-        <li key={technology.id} className="technology-item" onClick={() => initModal(technology)}>
-            <img src={`images/Technologies/${technology.image}`} alt={technology.alt} />
-            <h3>{technology.name}</h3>
-        </li>
-    ));
-    const newTechnologyItems = technology.newTech.map((technology) => (
-        <li key={technology.id} className="technology-item" onClick={() => initModal(technology)}>
-            <img src={`images/Technologies/${technology.image}`} alt={technology.alt} />
-            <h3>{technology.name}</h3>
-        </li>
-    ));
-    return(
-        <>
-            <section id='Technologies'>
-                <div>
-                    <h2>Technologies</h2>
-                    <p>Here are the technologies that I have expert experience in.</p>
-                    <ul>
-                        {knownTechnologyItems}
-                    </ul>
-                </div>
-                <div>
-                    <p>And here are the technologies that I am still learning in Novare.</p>
-                    <ul className="technologies-container">
-                        {newTechnologyItems}
-                    </ul>
-                </div>
-            </section>
-            <TechnologiesModal tech={modalTech} open={isOpen} onClose={() => setIsOpen(false)} />
-        </>
-    );
+  const knownTechnologyItems = technology.knownTech.map((technology) => (
+    <li
+      key={technology.id}
+      className="technology-item"
+      onClick={() => initModal(technology)}
+    >
+      <img
+        src={`images/Technologies/${technology.image}`}
+        alt={technology.alt}
+      />
+      <h3>{technology.name}</h3>
+    </li>
+  ));
+  const newTechnologyItems = technology.newTech.map((technology) => (
+    <li
+      key={technology.id}
+      className="technology-item"
+      onClick={() => initModal(technology)}
+    >
+      <img
+        src={`images/Technologies/${technology.image}`}
+        alt={technology.alt}
+      />
+      <h3>{technology.name}</h3>
+    </li>
+  ));
+  return (
+    <>
+      <section id="Technologies">
+        <div>
+          <h2>Technologies</h2>
+          <p>Here are the technologies that I have expert experience in.</p>
+          <ul>{knownTechnologyItems}</ul>
+        </div>
+        <div>
+          <p>
+            And here are the technologies that I have already learnt with Novare
+            Potential.
+          </p>
+          <ul className="technologies-container">{newTechnologyItems}</ul>
+        </div>
+      </section>
+      <TechnologiesModal
+        tech={modalTech}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+    </>
+  );
 }
